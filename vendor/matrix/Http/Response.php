b838cda7d4c0827be2c96ce2113ed950a95ed730
Elodie Bayet <ask@elodiebayet.com>
2025-09-04 14:23:44 +0200

    Corrige le traitement des en-têtes

--- vendor/matrix/Http/Response.php
+++ vendor/matrix/Http/Response.php
@@ -19,10 +19,7 @@ class Response
     ];
 
     private static $defaultHeaders = [
-        'Content-Type' => [
-            "text/html;", 
-            "charset=UTF-8",
-        ],
+        'Content-Type' => "text/html; charset=UTF-8",
     ];
 
     /** @var int $statusCode */
@@ -37,22 +34,20 @@ class Response
     /**
      * Build parts for Response
      *
-     * @param array $headers List of headers
      * @param string $body HTML body
+     * @param int $status
+     * @param array $headers List of headers
      */
     function __construct(string $body, int $status = self::HTTP_OK, array $headers = [])
     {
         $this->body = $body;
         $this->statusCode = $status;
-        $this->addHeaders($headers);
+        $this->headers = empty($headers) ? static::$defaultHeaders : $headers;
     }
 
-    private function addHeaders(array $headers): void
+    public function addHeaders(array $headers): void
     {
-        $this->headers = self::$defaultHeaders;
-        if (!empty($headers)) {
-            $this->headers = [...$this->headers, ...$headers];
-        }
+        $this->headers = array_merge($this->headers, $headers);
     }
 
     /** 
@@ -60,11 +55,10 @@ class Response
      */
     public function send(): void
     {
-        foreach ($this->headers as $name => $values) {
+        foreach ($this->headers as $name => $value) {
+            // In case of 'content-type' added in lowercase and duplicate entry in headers
             $replace = 0 === strcasecmp($name, 'Content-Type');
-            foreach ($values as $value) {
-                header($name.': '.$value, $replace, $this->statusCode);
-            }
+            header($name.': '.$value, $replace, $this->statusCode);
         }
 
         header(sprintf('HTTP/1.1 %s %s', $this->statusCode, self::$statusTexts[$this->statusCode]), true, $this->statusCode);
